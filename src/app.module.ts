609980b4147f6bf58511b288791301ee0569f21c
eliasfmartins <eliasmartinsdev8@gmail.com>
2025-09-15 22:25:03 -0300

    feat: atualizando imports de modulos

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaService } from './prisma/prisma.service';
+import { CreateAccountController } from './controllers/create-account.controller';
+import { AuthModule } from './auth/auth.module';
+import { TasksModule } from './tasks/tasks.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [AuthModule, TasksModule],
+  controllers: [AppController, CreateAccountController],
   providers: [AppService, PrismaService],
 })
 export class AppModule {}

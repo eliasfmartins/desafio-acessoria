5d51a09f8480d2cfb5a9e598d4da6b4c499f74d6
eliasfmartins <eliasmartinsdev8@gmail.com>
2025-09-16 09:39:11 -0300

    refactor(app): atualiza importações no módulo principal

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,13 +2,15 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaService } from './prisma/prisma.service';
-import { CreateAccountController } from './controllers/create-account.controller';
 import { AuthModule } from './auth/auth.module';
 import { TasksModule } from './tasks/tasks.module';
+import { TagsModule } from './tags/tags.module';
+import { AdminModule } from './admin/admin.module';
+import { StatsModule } from './stats/stats.module';
 
 @Module({
-  imports: [AuthModule, TasksModule],
-  controllers: [AppController, CreateAccountController],
+  imports: [AuthModule, TasksModule, TagsModule, AdminModule, StatsModule],
+  controllers: [AppController],
   providers: [AppService, PrismaService],
 })
 export class AppModule {}
